03f903c15bb59f4ab123246029ad6eeae6c0ee9b
jk31 <jarekkornowicz@googlemail.com>
2024-05-15 12:10:36 +0200

    renaming the request_logging_middleware

--- pslvis/settings.py
+++ pslvis/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "app.middleware.RequestLoggingMiddleware",
+    "app.request_logging_middleware.RequestLoggingMiddleware",
 ]
 
 ROOT_URLCONF = "pslvis.urls"
