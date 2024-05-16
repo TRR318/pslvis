fd649b9ed158a4110069deb85faba2228d3b6ade
Stefan <stefan.heid@lmu.de>
2024-05-16 16:34:13 +0200

    added basic dataset upload validation

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.core.exceptions import ValidationError
 from .models import Dataset
 
-# TODO check featurenames length with dataset
+import pandas as pd
+
 class DatasetForm(forms.ModelForm):
     class Meta:
         model = Dataset
@@ -10,6 +11,21 @@ class DatasetForm(forms.ModelForm):
 
     def clean_featurenames(self):
         features = self.cleaned_data.get('featurenames')
-        # if len(features) > 10:
-        #     raise ValidationError("The name must be at least 10 characters long.")
+        n_features = len(pd.read_csv(self.files.get("filepath",self.initial.get("filepath")).file, nrows=0).columns) -1
+                
+        if len(features) != n_features:
+            raise ValidationError(f"The featurenames list (len={len(features)}) must be one shorter than the column count ({n_features}) of the csv. Note that the first column is interpret as the target variable.")
         return features
+
+    def clean_filepath(self):
+        try:
+            bio = self.cleaned_data.get("filepath").file
+            bio.seek(0)
+            df = pd.read_csv(bio)
+        except pd.EmptyDataError:
+            raise ValidationError("Dataset apears to be empty")
+        except Exception:            
+            raise ValidationError("Appears not to be a csv")
+        
+        if not set(df.iloc[:,0]) <= {0,1}:
+            raise ValidationError("Column one must be the target column and only contain 0 and 1 values")
