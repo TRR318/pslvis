3c40ad758b55bb0b96b57973cd1ba1ded10ac40b
jk31 <jarekkornowicz@googlemail.com>
2024-05-16 22:20:12 +0200

    merged branch and fixed subject and experiment constructors

--- app/models/experiment.py
+++ app/models/experiment.py
@@ -4,7 +4,7 @@ from .dataset import Dataset
 
 
 class Experiment(models.Model):
-    id = models.SlugField(primary_key=True, default=token_urlsafe(8), editable=False)
+    id = models.SlugField(primary_key=True, default=lambda: token_urlsafe(4), editable=False)
     dataset = models.ForeignKey(
         Dataset,
         verbose_name="Dataset attached to the Experiment",
