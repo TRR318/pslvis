42e0b0715fad723e63bb15e2bb83b04aa206ee94
Stefan <stefan.heid@lmu.de>
2024-05-23 12:44:18 +0200

    Small fix on document ready and some htmx refactoring and fixes

--- app/urls.py
+++ app/urls.py
@@ -1,21 +1,25 @@
-from django.urls import path
+from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
-    # TODO create new experiment from get request
-    #path('ex', views.create_subject, name='Create subject'),
-    path('ex/<ex_id>', views.create_subject, name='Create subject'),
 
-    path('ex/<subj_id>/', views.index, name='index'),
-    # TODO this function will be called when a button in the history is clicked. it will produce a result like update-table with no changes
-    path('ex/<subj_id>/get', views.get, name='get-model'),
-    path('ex/<subj_id>/update-table', views.update_table, name='update-table'),
-    path('ex/<subj_id>/reset', views.reset, name='reset'),
-    path('ex/<subj_id>/add', views.add, name='add'),
-    path('ex/<subj_id>/fill', views.fill, name='fill'),
-    
-    path('ex/<subj_id>/save', views.save_model, name='save'),
+    path('ex/', include([
+        # TODO create new experiment from get request
+        # path('ex', views.create_subject, name='Create subject'),
+        path('<ex_id>', views.create_subject, name='Create subject'),
 
-    # TODO allow deleting models by a delete request. it will actually only be marked as "deleted" and not removed from db
-    # https://htmx.org/attributes/hx-delete/
+        path('<subj_id>/', views.index, name='index'),
+        # TODO this function will be called when a button in the history is clicked. it will produce a result like update-table with no changes
+        path('<subj_id>/get', views.get, name='get-model'),
+        path('<subj_id>/update-table', views.update_table, name='update-table'),
+        path('<subj_id>/reset', views.reset, name='reset'),
+        path('<subj_id>/add', views.add, name='add'),
+        path('<subj_id>/fill', views.fill, name='fill'),
+
+        path('<subj_id>/save', views.save_model, name='save'),
+
+        # TODO allow deleting models by a delete request. it will actually only be marked as "deleted" and not removed from db
+        # https://htmx.org/attributes/hx-delete/
+    ]))
 ]
