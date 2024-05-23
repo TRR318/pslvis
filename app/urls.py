cde9f4fcce0397cae2e15ccd9b1e7f738b4f6d25
Stefan <stefan.heid@lmu.de>
2024-05-23 08:04:44 +0200

    saving model to history works. next is loading and deletion

--- app/urls.py
+++ app/urls.py
@@ -2,13 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # TODO
+    # TODO create new experiment from get request
     #path('ex', views.create_subject, name='Create subject'),
     path('ex/<ex_id>', views.create_subject, name='Create subject'),
+
     path('ex/<subj_id>/', views.index, name='index'),
-    path('ex/<subj_id>/update-table', views.update_table, name='update-_table'),
+    # TODO this function will be called when a button in the history is clicked. it will produce a result like update-table with no changes
+    path('ex/<subj_id>/get', views.get, name='get-model'),
+    path('ex/<subj_id>/update-table', views.update_table, name='update-table'),
     path('ex/<subj_id>/reset', views.reset, name='reset'),
     path('ex/<subj_id>/add', views.add, name='add'),
     path('ex/<subj_id>/fill', views.fill, name='fill'),
-    path('ex/<subj_id>/history', views.fill, name='fill'),
+    
+    path('ex/<subj_id>/save', views.save_model, name='save'),
+
+    # TODO allow deleting models by a delete request. it will actually only be marked as "deleted" and not removed from db
+    # https://htmx.org/attributes/hx-delete/
 ]
