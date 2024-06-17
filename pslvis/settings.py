a204d19f10bcb0087b48843b8760b77d9bbfe09c
jk31 <jarekkornowicz@googlemail.com>
2024-06-17 14:43:09 +0200

    added default storage

--- pslvis/settings.py
+++ pslvis/settings.py
@@ -194,7 +194,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STORAGES = {
-    # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
