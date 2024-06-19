bab4e6bff8b0518c1c8b686915fccac49f46fa5e
Stefan <stefan.heid@lmu.de>
2024-06-19 10:39:10 +0200

    expose SECRET_KEY as an env variable

--- pslvis/settings.py
+++ pslvis/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#$_9lq!0!_x5j6^12z)m81_h0ftfdc#$pxu2$5@g=ao=w=-1nt"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
