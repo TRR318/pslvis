3060e6d7aa609166a3b50def102c3c6c190ebe2e
jk31 <jarekkornowicz@googlemail.com>
2024-06-21 11:24:30 +0200

    removed outcommented code, removed text

--- app/models/dataset.py
+++ app/models/dataset.py
@@ -3,19 +3,6 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django import forms
 
-# class Dataset(models.Model):
-#     name = models.CharField(primary_key=True, max_length=30)
-#     friendlyname = models.CharField(verbose_name="Friendly name of the Dataset", max_length=30)
-#     featurenames = models.JSONField(verbose_name="Friendly feature names as a json-list", default=list)
-#     filepath = models.FileField(verbose_name="Dataset as csv. First column must be target", upload_to="dataset/")
-
-#     def __str__(self):
-#         return self.friendlyname
-    
-#     @property
-#     def path(self):
-#         return self.filepath.path
-
 
 class Dataset(models.Model):
     name = models.CharField(primary_key=True, max_length=30)
