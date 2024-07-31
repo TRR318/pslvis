aba5006d17a7bf9834c69edfcf6ec03c53c2c1b0
jk31 <jarekkornowicz@googlemail.com>
2024-07-31 16:26:38 +0200

    integrating experimental parameters for layout and fixing border

--- app/static/js/initialize.js
+++ app/static/js/initialize.js
@@ -2,23 +2,23 @@ import { sortableStaff } from "./sortable_staff.js";
 import { heatmap } from "./heatmap.js";
 
 function initialize() {
-    // Send height to iframe parent
-    window.parent.postMessage({ height: document.body.scrollHeight }, '*');
-    window.onload = () => window.parent.postMessage({ new_url: window.location.href }, '*');
+  // Send height to iframe parent
+  window.parent.postMessage({ height: document.body.scrollHeight }, '*');
+  window.onload = () => window.parent.postMessage({ new_url: window.location.href }, '*');
 
-    // fix to delete orphaned tooltips
-    document.querySelectorAll(".tooltip.bs-tooltip-auto").forEach(e => e.remove())
-    document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(tr => new bootstrap.Tooltip(tr))
+  // fix to delete orphaned tooltips
+  document.querySelectorAll(".tooltip.bs-tooltip-auto").forEach(e => e.remove())
+  document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(tr => new bootstrap.Tooltip(tr))
 
-    // if ("chart" in window) {
-    //     chart.data.labels = labels;
-    //     chart.data.datasets[0].data = data;
-    //     chart.update();
-    // }
+  if ("chart" in window) {
+    chart.data.labels = labels;
+    chart.data.datasets[0].data = data;
+    chart.update();
+  }
 
-    sortableStaff();
+  sortableStaff();
 
-    heatmap();
+  heatmap();
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
